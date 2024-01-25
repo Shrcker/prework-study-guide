88996b5eeed60db41e51d08ab498ad7457eb1374
Tania <tanner.shirkey@gmail.com>
2024-01-25 15:22:53 -0500

    Added JavaScript functionality and js code that iterates a simple array

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,20 @@
+// var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+// array for the study guide topics. Will iterate in for loop.
+
+for (let i = 0; i < 4; i++) {
+    console.log(topics[i]);
+}
+/*
+if (topic === 'HTML') {
+    console.log ("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS");
+} else if (topic === 'Git') {
+    console.log("Let's study Git");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
