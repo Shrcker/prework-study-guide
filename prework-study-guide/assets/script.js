3d3313aa837c124b721aa17a4ef505eb37809d67
Tania <tanner.shirkey@gmail.com>
2024-01-26 15:35:42 -0500

    Fully implemented JavaScript to randomly select a topic

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,20 +1,27 @@
-// var topic = "HTML";
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-// array for the study guide topics. Will iterate in for loop.
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let i = 0; i < 4; i++) {
-    console.log(topics[i]);
+function listTopics() {
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
 }
-/*
-if (topic === 'HTML') {
-    console.log ("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS");
-} else if (topic === 'Git') {
-    console.log("Let's study Git");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log('Please try again!');
+
+function selectTopic() {
+    if (randomTopic=== 'HTML') {
+        console.log ("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
-*/
+
+console.log('Here are the topics I learned through Prework:');
+listTopics();
+console.log('What should I study today?');
+selectTopic();
